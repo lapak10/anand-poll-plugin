01d4496eb28b398c04b70150c81180b8c505dc12
Anand Kumar <anand.kmk@gmail.com>
2019-07-15 20:20:12 +0530

    init

---
+++ anand-poll-plugin.php
@@ -0,0 +1,9 @@
+<?php defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+/**
+* Plugin Name: Anand WP Poll
+* Plugin URI: https://www.example.com
+* Description: This is a sample poll plugin for Command Media. Submitted by anand.kmk@gmail.com
+* Version: 1.0
+* Author: Anand - anand.kmk@gmail.com
+* Author URI: https://example.com
+**/
